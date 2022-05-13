eba2b5e5cbca36e632e20e58cf59b992a426dec7
dyang46 <dyang46@ucsc.edu>
2022-05-12 20:20:50 -0700

    map

---
+++ src/scenes/Map.js
@@ -0,0 +1,11 @@
+class Map extends Phaser.Scene {
+    constructor(scene, mapkey, texture) {
+        map = scene.make.tilemap({key: mapkey});
+        var tileset = map.addTilesetImage(texture);
+    }
+
+    
+
+    update() {
+        
+    }
