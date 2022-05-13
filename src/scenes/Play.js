8092ebfe2b490be34235dcdca7567502388a277d
CallMeCalvin101 <calvinkurniadjaja@gmail.com>
2022-05-13 13:25:28 -0700

    Camera Fix + Player Attack Fix

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -43,16 +43,16 @@ class Play extends Phaser.Scene {
 
         // Adds Pointer Down Event for Player Attacks
         this.input.on('pointerdown', () => {
-            this.player.attack(gamePointer.x, gamePointer.y);
+            this.player.attack(gamePointer.worldX, gamePointer.worldY);
         }, this);
 
         this.checkUpgrade();
         
 
         // camera
-        const camera = this.cameras.main;
-        camera.startFollow(this.player);
-        
+        this.camera = this.cameras.main;
+        this.camera.startFollow(this.player);
+        this.camera.setBounds(0, 0, 3000, 3000);
     }
 
 
@@ -74,6 +74,7 @@ class Play extends Phaser.Scene {
             this.spawnY = this.player.y;
             this.player.destroy();
             this.player = new PlayerBody(this, this.spawnX, this.spawnY);
+            this.camera.startFollow(this.player);
             elem.destroy();
         }
     }
